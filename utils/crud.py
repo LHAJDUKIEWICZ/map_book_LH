de6958b65e851b97862cadaf8bf36a62fdc6c68b
LHAJDUKIEWICZ <167193876+LHAJDUKIEWICZ@users.noreply.github.com>
2024-04-23 15:17:50 +0200

    feat: remove was added

--- utils/crud.py
+++ utils/crud.py
@@ -1,6 +1,7 @@
-def read(users: list[dict[str, str]])->None:
+def read(users: list[dict[str, str]]) -> None:
     for user in users[1:]:
         print(f"Twój znajomy {user['name']} opublikował {user['posts']} posty")
+
 
 def add_user(lista: list) -> None:
     user_name = input('Podaj imię: ')
@@ -8,3 +9,17 @@ def add_user(lista: list) -> None:
     user_posts = input('Podaj ilość postów: ')
     lista.append({"name": user_name, "surname": user_surname, "posts": user_posts})
 
+    def search(users: list[dict[str, str]]) -> None:
+        user_name = input('Kogo szukasz?: ')
+        for user in users[1:]:
+            if user['name'] == user_name:
+                print(f"Znaleziono użytkownika {user}")
+
+    search(users)
+
+    def remove_user(users: list[dict[str, str]]) -> None:
+        user_name = input('Kogo usunąć?: ')
+        for user in users[1:]:
+            if user['name'] == user_name:
+                print(f"Znaleziono użytkownika {user['name']}")
+                user.remove(user)
